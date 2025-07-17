e1e71f6f872c16a884b6483dbde48af5b98e7fbf
ira_trubova <iraraddionova@gmail.com>
2025-07-17 23:07:34 +0200

    Initial Commit

---
+++ Landmarks/LandmarksApp.swift
@@ -0,0 +1,17 @@
+//
+//  LandmarksApp.swift
+//  Landmarks
+//
+//  Created by Iryna Radionova on 17.07.25.
+//
+
+import SwiftUI
+
+@main
+struct LandmarksApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
